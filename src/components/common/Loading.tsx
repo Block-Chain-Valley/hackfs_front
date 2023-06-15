c2f44b2002ff55fefcd3ea1559affc53cbf86213
mangming2 <leejiho35@gmail.com>
2023-06-15 23:14:16 +0900

    Update Loading.tsx

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -4,7 +4,7 @@ const Loading: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-8 border-blue-500"></div>
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
       </div>
     </div>
   );
