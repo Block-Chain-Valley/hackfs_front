ab36f1f0158db975917071f878db4337a9023dcd
mangming2 <leejiho35@gmail.com>
2023-06-15 23:26:23 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./index.css";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { RecoilRoot } from "recoil";
-import { HeliaProvider } from "../src/provider/HeliaProvider";
 
 declare global {
   interface Window {
